3d7c7f3034883c3de5b2f00d7c77fa1d1e586ac3
vijay711-github <vj7112000@gmail.com>
2023-08-06 00:28:53 +0530

    Completed Navbar and Footer component design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex item-center justify-between'>
+      <Link href="/" className='font-bold text-xl'>EATMEDI</Link>
+      <p>&copy;All Rights Reserved</p>
+    </div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
-      <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">EatMe</Link>
-      </div>
-      <div>
-        <Menu/>
-      </div>
-    </div>
-  )
-}
+	const user = false;
+	return (
+		<div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40 ">
+			<div className="hidden md:flex gap-4">
+				<Link href="/">Homepage</Link>
+				<Link href="/menu">Menu</Link>
+				<Link href="/contact">Contact</Link>
+			</div>
+			<div className="text-xl md:font-bold flex-1 md:text-center">
+				<Link href="/">EatMe</Link>
+				di
+			</div>
+			<div className="md:hidden">
+				<Menu />
+			</div>
+			<div className="hidden md:flex gap-4 item-center justify-end flex -1">
+				<div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+					<Image src="/phone.png" alt="" width={20} height={20} />
+					<span>123 222 3333 </span>
+				</div>
+				{!user ? (
+					<Link href="/login">Loginpage</Link>
+				) : (
+					<Link href="/orders">Orders</Link>
+				)}
+				<CartIcon/>
+			</div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
