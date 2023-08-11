3f41b33e180d4bae7b0826ebc855b8a5608926f5
vijay711-github <vj7112000@gmail.com>
2023-08-12 00:45:35 +0530

    Designed Menu page and added menu cards

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import React from 'react'
-import CountDown from './CountDown'
+import Image from "next/image";
+import React from "react";
+import CountDown from "./CountDown";
 const Offer = () => {
-  return (
+	return (
 		<div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
 			{/* {Text Conatiner} */}
 			<div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
@@ -24,6 +24,6 @@ const Offer = () => {
 			</div>
 		</div>
 	);
-}
+};
 
-export default Offer
+export default Offer;
