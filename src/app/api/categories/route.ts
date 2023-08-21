07a9df6db9dfe1aa5245d019142efaf73b591776
vijay711-github <vj7112000@gmail.com>
2023-08-21 13:19:46 +0530

    Integrate with Prisma DB and created DB Tables and Dockerization

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+import { NextResponse } from "next/server"
+
+const prisma = new PrismaClient()
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(JSON.stringify(categories), { status: 200 })
+    } catch (error) {
+        console.log(error)
+        return new NextResponse(JSON.stringify({message:"something went wrong"}),{status:500})
+
+    }
+}
+export const POST = () => {
+    return new NextResponse("Hello",{status:200})
+}
--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,17 +1,33 @@
 "use client";
-import React from "react";
-import Countdown from "react-countdown";
 
-const endingdate = new Date("2023-10-30");
+import React, { useState, useEffect } from "react";
 
 const CountDown = () => {
+	let difference = +new Date(`10/10/2023`) - +new Date();
+	const [delay, setDelay] = useState(difference);
+
+	const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+	const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
+	const m = Math.floor((difference / 1000 / 60) % 60);
+	const s = Math.floor((difference / 1000) % 60);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setDelay(delay - 1);
+		}, 1000);
+
+		if (delay === 0) {
+			clearInterval(timer);
+		}
+
+		return () => {
+			clearInterval(timer);
+		};
+	});
 	return (
-		<div className="text-white">
-			<Countdown
-				className="font-bold text-5xl text-yellow-300"
-				date={endingdate}
-			/>
-		</div>
+		<span className="font-bold text-5xl text-yellow-300">
+			{d}:{h}:{m}:{s}
+		</span>
 	);
 };
 
