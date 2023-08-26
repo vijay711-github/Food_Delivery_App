6f1c7791f69040e8d505f8b8051effda1655ce67
vijay711-github <vj7112000@gmail.com>
2023-08-26 12:34:54 +0530

    added store using Zustand and completed the cart page user can add or remove items

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,16 +1,21 @@
+"use client"
+
+import { useCartStore } from '@/utils/store'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const CartIcon = () => {
+    const { totalItems } = useCartStore();
+
   return (
-      <Link href="/cart" className='flex items-center gap-4'>
-          <div className='relative w-8 h-8 md:w-5 md:h-5'>
-              <Image src="/cart.png" alt="cart-image" fill/>
-          </div>
-          <span>Cart(3)</span>
-    </Link>
-  )
+		<Link href="/cart" className="flex items-center gap-4">
+			<div className="relative w-8 h-8 md:w-5 md:h-5">
+				<Image src="/cart.png" alt="cart-image" fill />
+			</div>
+			<span>Cart({totalItems})</span>
+		</Link>
+	);
 }
 
 export default CartIcon
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { ProductType } from "@/types/types";
+import { useCartStore } from "@/utils/store";
 import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+
 
 const Price = ({ product }: { product: ProductType }) => {
 	const [total, setTotal] = useState(product.price);
@@ -13,6 +16,20 @@ const Price = ({ product }: { product: ProductType }) => {
 			);
 		}
 	}, [quantity, selected, product]);
+	const { addToCart } = useCartStore();
+	 const handleCart = () => {
+			addToCart({
+				id: product.id,
+				title: product.title,
+				img: product.img,
+				price: total,
+				...(product.options?.length && {
+					optionTitle: product.options[selected].title,
+				}),
+				quantity: quantity,
+			});
+			toast.success("The product added to the cart!");
+		};
 
 	return (
 		<div className="flex flex-col gap-4">
@@ -56,7 +73,10 @@ const Price = ({ product }: { product: ProductType }) => {
 					</div>
 				</div>
 				{/* CART BUTTON */}
-				<button className="uppercase w-56  bg-red-500 p-3 text-white ring-1 ring-red-400">
+				<button
+					className="uppercase w-56  bg-red-500 p-3 text-white ring-1 ring-red-400"
+					onClick={handleCart}
+				>
 					Add to Cart
 				</button>
 			</div>
