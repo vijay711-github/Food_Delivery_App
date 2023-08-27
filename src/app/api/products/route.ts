ac6b518f378769dbf9d7a97bb7f878f4f06c4b5a
vijay711-github <vj7112000@gmail.com>
2023-08-27 16:53:57 +0530

    Added Stripe api for payment integration and completed all pages

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,23 +1,38 @@
-import { prisma } from "@/utils/connect"
-import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/utils/connect";
+import { NextRequest, NextResponse } from "next/server";
 
+// FETCH ALL PRODUCTS
 export const GET = async (req: NextRequest) => {
-    const { searchParams } = new URL(req.url)
-      const cat = searchParams.get("cat");
+  const { searchParams } = new URL(req.url);
+  const cat = searchParams.get("cat");
 
-    try {
-        const products = await prisma.product.findMany({
+  try {
+    const products = await prisma.product.findMany({
       where: {
         ...(cat ? { catSlug: cat } : { isFeatured: true }),
       },
-    })
-        return new NextResponse(JSON.stringify(products), { status: 200 })
-    } catch (error) {
-        console.log(error)
-        return new NextResponse(JSON.stringify({message:"something went wrong"}),{status:500})
-
-    }
-}
-export const POST = () => {
-    return new NextResponse("Hello",{status:200})
-}
+    });
+    return new NextResponse(JSON.stringify(products), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    const product = await prisma.product.create({
+      data: body,
+    });
+    return new NextResponse(JSON.stringify(product), { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from "@/components/DeleteButton";
 import Price from "@/components/Price";
 import { ProductType } from "@/types/types";
 import Image from "next/image";
@@ -15,32 +16,33 @@ const getData = async (id: string) => {
 	return res.json();
 };
 
-const ProductPage = async ({ params }: { params: { id: string } }) => {
+const SingleProductPage = async ({ params }: { params: { id: string } }) => {
 	const singleProduct: ProductType = await getData(params.id);
 
 	return (
-		<div className="p-4 lg:px-20 lx:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
+		<div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center relative">
 			{/* IMAGE CONTAINER */}
 			{singleProduct.img && (
-				<div className="relative w-full h-1/2 md:h[70%]">
+				<div className="relative w-full h-1/2 md:h-[70%]">
 					<Image
 						src={singleProduct.img}
 						alt=""
-						className="object-contain hover:rotate-12"
+						className="object-contain"
 						fill
 					/>
 				</div>
 			)}
 			{/* TEXT CONTAINER */}
-			<div className="h-1/2 flex flex-col gap-4 md:h[70%] md:justify-center md:gap-6 xl:gap-8">
-				<h1 className="font-bold text-3xl uppercase xl:text-5xl">
-					{singleProduct.title}
+			<div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
+				<h1 className="text-3xl font-bold uppercase">
+					<span>{singleProduct.title}</span>
+					<DeleteButton id={singleProduct.id} />
 				</h1>
 				<p>{singleProduct.desc}</p>
-				<Price product ={singleProduct}/>
+				<Price product={singleProduct} />
 			</div>
 		</div>
 	);
 };
 
-export default ProductPage;
+export default SingleProductPage;
